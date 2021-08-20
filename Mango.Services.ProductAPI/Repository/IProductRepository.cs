371b9b593f8dfe207c55db1a95272543722d2ca1
Adam M <amathison@crankyape.com>
2021-08-20 16:02:29 -0500

    Initial Commit

---
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetProductById(int productId);
+        Task<ProductDTO> CreateUpdateProduct(ProductDTO productDto);
+        Task<bool> DeleteProduct(int productId); 
+    }
+}
